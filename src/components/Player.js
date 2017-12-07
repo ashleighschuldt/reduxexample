02f467786b6d783f9664aaf05ebb392263661ed5
Joshua Richard Smith <veryjoshua@gmail.com>
2017-12-06 18:50:22 -0700

    changes

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,21 +1,29 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-
+import { bindActionCreators} from 'redux';
+import {Change_Current_Player} from '../actions/action';
 
 class Player extends Component {
   onNewPlayer(e) {
-    this.props.dispatch({ type: 'NEW_PLAYER', payload: this.refs.newPlayer.value })
+    this.props.Change_Current_Player(this.refs.newPlayer.value);
   }
 
   render() {
     return (
       <div>
         <h2>Current player: {this.props.currentPlayer}</h2>
-        <input ref='newPlayer' placeHolder='type new player' type="text" />
+        <input ref='newPlayer' placeholder='type new player' type="text" />
         <button onClick={this.onNewPlayer.bind(this)}>enter new player</button>
+
       </div>
     );
   }
 }
+function mapStateToProps({currentPlayer}){
+  return {currentPlayer};
+}
+function mapDispatchToProps(dispatch){
+  return bindActionCreators({Change_Current_Player}, dispatch);
+}
 
-export default connect(state => state)(Player);
+export default connect(mapStateToProps, mapDispatchToProps)(Player);
